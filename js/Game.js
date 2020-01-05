f21eb7292873b8178d4cdaacfec83f5e3881f896
DeansRepos <54600923+DeanYRepos@users.noreply.github.com>
2020-01-05 15:17:38 -0600

    populated classes with some properties

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,18 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+constructor(){
+this.missed = 0;
+this.phrases = createPhrase();
+this.activePhrase = null;
+
+
+}
+createPhrase() {
+
+    return ['The finger thing means the taxes',''];
+}
+
+ };
