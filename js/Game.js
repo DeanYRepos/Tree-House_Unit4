ee22e10953cabda2578c49f4870f2b033950660e
DeansRepos <54600923+DeanYRepos@users.noreply.github.com>
2020-01-06 17:00:15 -0600

    createPhrases() method populated and assigned to phrases property in Game class

--- js/Game.js
+++ js/Game.js
@@ -5,14 +5,40 @@
  class Game {
 constructor(){
 this.missed = 0;
-this.phrases = createPhrase();
+this.phrases =  this.createPhrases();
+
 this.activePhrase = null;
 
-
 }
-createPhrase() {
 
-    return ['The finger thing means the taxes',''];
-}
+createPhrases() {
+
+let phraseArray = [
+    {
+phrase:'The finger thing means the taxes'
+
+    },    {
+        phrase:'I think therefore I am'
+        
+            },    {
+                phrase:'I find your lack of faith disturbing'
+                
+                    },    {
+                        phrase:'I knew it was wrong I did it anyway'
+                        
+                            },    {
+                                phrase:'Stupid is a stupid does'
+                                
+                                    }
+
+
+
+
+];
+
+
+
+    return phraseArray;
+};
 
  };
--- js/app.js
+++ js/app.js
@@ -3,7 +3,16 @@
  * app.js */
 
 
-// const game = new Game();
- const phrase = new Phrase ("I know not fear");
+const game = new Game();
+// const phrase = new Phrase();
 
- console.log(`Phrase - phrase: ${phrase.phrase}`);
+ //console.log(`Phrase - phrase: ${phrase.phrase}`);
+game.phrases.forEach((phrase, index) =>{
+
+
+
+
+    
+console.log(`Phrase ${index} - phrase ${phrase.phrase}`);
+
+});
