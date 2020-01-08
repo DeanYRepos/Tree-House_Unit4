d6dda055f84062e2ebbe1e57d281b82504dc0086
DeansRepos <54600923+DeanYRepos@users.noreply.github.com>
2020-01-08 16:09:18 -0600

    current code debugging

---
+++ app.js
@@ -0,0 +1,46 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * app.js */
+
+
+
+//const phrase = new Phrase();
+
+ //console.log(`Phrase - phrase: ${phrase.phrase}`);
+// game.phrases.forEach((phrase, index) =>{
+
+
+
+
+    
+// console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+
+//});
+// const logPhrase = (phrase) => {
+// console.log(`Phrase - phrase: `, phrase.phrase);
+//  };
+//   const game = new Game();
+// const randomPhrase = game.getRandomPhrase();
+// const phrase = new Phrase(randomPhrase.phrase);
+
+//const phrase3 = new Phrase('FUCK YOU');
+// const phrase2 = new Phrase();
+//console.log(phrase3)
+// phrase.addPhraseToDisplay();
+
+//  logPhrase(game.getRandomPhrase());
+
+//  game = new Game();
+//  game.startGame();
+// logPhrase(game.getRandomPhrase());
+// logPhrase(game.getRandomPhrase());
+// logPhrase(game.getRandomPhrase());
+// logPhrase(game.getRandomPhrase());
+// logPhrase(game.getRandomPhrase());
+
+const game =new Game();
+const randomPhrase = game.getRandomPhrase();
+const phrase = new Phrase (randomPhrase.phrase);
+//const phrase2 = new Phrase(this.phrase);
+
+phrase.addPhraseToDisplay(randomPhrase);
