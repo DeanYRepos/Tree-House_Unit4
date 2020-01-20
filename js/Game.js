fef2371bf9045329db913ea87fd48c80409c3b03
DeansRepos <54600923+DeanYRepos@users.noreply.github.com>
2020-01-19 20:48:18 -0600

    change to gameOver() method

--- js/Game.js
+++ js/Game.js
@@ -46,44 +46,84 @@ class Game {
 
     }
 
-    startGame() { 
+    startGame() {
 
 
         let overlay = document.getElementById('overlay'); //calls overlay id
         overlay.style.visibility = 'hidden'; //hides overlay 
-        this.activePhrase = this.getRandomPhrase();  //sets random phrase to active phrase
-        this.activePhrase.addPhraseToDisplay();         //displays active phrase to display
+        this.activePhrase = this.getRandomPhrase(); //sets random phrase to active phrase
+        this.activePhrase.addPhraseToDisplay(); //displays active phrase to display
 
     }
-    handleInteraction(){
+    handleInteraction() {
 
 
 
     }
-    checkForWin(){
+    checkForWin() {
 
-const allLetters =document.querySelectorAll('.letter');
-const numLetters = allLetters.length;
-const shownLetters = document.querySelectorAll('.show');
-const guessLetters = shownLetters.length;
+        const allLetters = document.querySelectorAll('.letter');
+        const numLetters = allLetters.length;
+        const shownLetters = document.querySelectorAll('.show');
+        const guessLetters = shownLetters.length;
 
 
-        if(numLetters === guessLetters){
+        if (numLetters === guessLetters) {
 
             return true;
 
         } else return false;
-        
 
-    
-    }
-    removeLife(){
-        
+
 
     }
-    gameOver(){
+    removeLife() {
 
-
+        const scoreBoard = document.querySelectorAll('li img');
+        this.missed += 1;
         
+
+        for (let i = 0; i < this.missed; i++) {
+
+                scoreBoard[i].src = "images/lostHeart.png";
+                console.log(this.missed);
+          
+        }
+        if (this.missed === 5) {
+
+            this.gameOver(false);
+        }
+
+    }
+
+
+
+
+
+
+
+    gameOver(gameWin) {
+        let overlay = document.getElementById('overlay');
+        const gameOverMessage = document.getElementById('game-over-message');
+        if (gameWin) {
+            overlay.classList.remove('start');
+            overlay.classList.add('win');
+
+            overlay.style.display = 'visible';
+            gameOverMessage.textContent = 'Congratulations, You Win!';
+
+
+        } else {
+
+
+            overlay.classList.remove('start');
+            overlay.classList.add('lose');
+
+            overlay.style.display = 'visible';
+            gameOverMessage.textContent = 'You Lose! Try again.';
+
+        }
+
+
     }
 };
