031b17a0782d736930d6835b38d78e644938bd72
DeansRepos <54600923+DeanYRepos@users.noreply.github.com>
2020-01-26 12:37:20 -0600

    Added comments to Game.js and Phrase.js

--- js/app.js
+++ js/app.js
@@ -11,19 +11,19 @@ const button = document.getElementById('btn__reset');
 button.addEventListener('click', () => { //eventlistener activates new game when start button is clicked
 
     game = new Game();
-   // game.reset();
+
     game.startGame();
 
 
 })
 
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => {
-    key.addEventListener('click', (e) => {
+const keys = document.querySelectorAll('.key'); 
+keys.forEach(key => {                       //iterates over keys
+    key.addEventListener('click', (e) => { //listens for clicks on targeted keys
 
 
-        game.handleInteraction(e.target);
+        game.handleInteraction(e.target); //callls handleInteration method and passes targeted keys
 
 
     })
